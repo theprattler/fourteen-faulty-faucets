b429d36a9664f4da0dd4decab776aa7c43825cd0
The Prattler <jmichaelsmith@juno.com>
2022-01-18 12:08:30 -0500

    used bcrypt to ensure password security

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -49,9 +49,32 @@ router.post('/', (req, res) => {
   });
 });
 
+// LOGIN route
+router.post('/login', (req, res) => {
+  User.findOne({
+    where: {
+      username: req.body.username
+    }
+  })
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that username' });
+      return;
+    }
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password' });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in' });
+  });
+});
+
 // UPDATE user
 router.put('/:id', (req, res) => {
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id
     }
