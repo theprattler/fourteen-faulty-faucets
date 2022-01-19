5d4c9d50e6a50a1e8779a6d450def6c4ae644a1f
The Prattler <jmichaelsmith@juno.com>
2022-01-19 14:16:09 -0500

    login and logout functionality

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -56,14 +56,22 @@ router.post('/', (req, res) => {
     username: req.body.username,
     password: req.body.password
   })
-  .then(dbUserData => res.json(dbUserData))
+  .then(dbUserData => {
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json(dbUserData);
+    });
+  })
   .catch(err => {
     console.log(err);
     res.status(500).json(err);
   });
 });
 
-// LOGIN route
+// LOGIN user
 router.post('/login', (req, res) => {
   User.findOne({
     where: {
@@ -80,9 +88,24 @@ router.post('/login', (req, res) => {
       res.status(400).json({ message: 'Incorrect password' });
       return;
     }
-
-    res.json({ user: dbUserData, message: 'You are now logged in' });
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn  = true;
+      res.json({ user: dbUserData, message: 'You are now logged in' });
+    });
   });
+});
+
+// LOGOUT user
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 // UPDATE user
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
+  console.log(req.session);
   Post.findAll({
     attributes: [
       'id',
@@ -31,7 +32,6 @@ router.get('/', (req, res) => {
       }
     ]
   })
-  
   .then(dbPostData => {
     const posts = dbPostData.map(post => post.get({ plain: true }))
     res.render('homepage', { posts });
@@ -40,6 +40,14 @@ router.get('/', (req, res) => {
     console.log(err);
     res.status(500).json(err);
   });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
 });
 
 module.exports = router;
