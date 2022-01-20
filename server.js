69793c4a5be2562c4ec6cb5320e683a057ab761b
The Prattler <jmichaelsmith@juno.com>
2022-01-20 11:19:32 -0500

    date displays more user friendly

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const session = require('express-session');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
