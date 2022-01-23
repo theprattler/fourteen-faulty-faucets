5b4b5db3d07cc3751010cc6c344349dfe912f9d8
The Prattler <jmichaelsmith@juno.com>
2022-01-23 15:53:14 -0500

    added more styling

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -52,7 +52,7 @@ router.get('/:id', (req, res) => {
 });
 
 // CREATE user
-router.post('/', withAuth, (req, res) => {
+router.post('/', (req, res) => {
   User.create({
     username: req.body.username,
     password: req.body.password
